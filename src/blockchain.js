63bb13a2e67a3d944a7629d81afd04d3ff850eb0
Jaecheol Shin <jcgod413@gmail.com>
2018-04-04 22:25:52 +0900

    Implement createNewBlock function and add gitignore file

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,3 +1,5 @@
+const CryptoJS = require('crypto-js');
+
 class Block {
   constructor(index, hash, previousHash, timestamp, data) {
     this.index = index;
@@ -12,10 +14,30 @@ const genesisBlock = new Block(
   0,
   "399DE39BFEA6784E4BDE5FF097D085A21BB4EA5148575051D988B6B74D68B082",
   null,
-  new Date().getTime(),
+  1522846996703,
   "This is the genesis block!"
 );
 
 let blockchain = [genesisBlock];
 
-console.log(blockchain);
+const getLastBlock = () => blockchain[blockchain.length - 1];
+
+const getTimestamp = () => new Date().getTime() / 1000;
+
+const createHash = (index, previousHash, timestamp, data) =>
+  CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
+const createNewBlock = data => {
+  const previousBlock = getLastBlock();
+  const newBlockIndex = previousBlock.index + 1;
+  const newTimestamp = getTimestamp();
+  const newHash = createHash(newBlockIndex, previousBlock.hash, newTimestamp, data);
+  const newBlock = new Block(
+    newBlockIndex,
+    newHash,
+    previousBlock.hash,
+    newTimestamp,
+    data
+  );
+  return newBlock;
+}
