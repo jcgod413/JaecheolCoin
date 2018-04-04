6e47562b5e0619c05a1f3e0b1241adc307b840ff
Jaecheol Shin <jcgod413@gmail.com>
2018-04-04 22:03:44 +0900

    Creating the Genesis Blcok

--- src/blockchain.js
+++ src/blockchain.js
@@ -7,3 +7,15 @@ class Block {
     this.data = data;
   }
 }
+
+const genesisBlock = new Block(
+  0,
+  "399DE39BFEA6784E4BDE5FF097D085A21BB4EA5148575051D988B6B74D68B082",
+  null,
+  new Date().getTime(),
+  "This is the genesis block!"
+);
+
+let blockchain = [genesisBlock];
+
+console.log(blockchain);
