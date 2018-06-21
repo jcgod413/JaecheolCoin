8b78c0b439cd8a37bf1f3f4407695ad00d1b895d
Jaecheol Shin <jcgod413@gmail.com>
2018-06-21 15:17:05 +0900

    Change http url

--- explorer/src/constants.js
+++ explorer/src/constants.js
@@ -1,2 +1,2 @@
-export const API_URL = "http://localhost:4000";
-export const WS_URL = "ws://localhost:4000";
+export const API_URL = "http://jcgod.sullivan.kr:4000";
+export const WS_URL = "ws://jcgod.sullivan.kr:4000";
